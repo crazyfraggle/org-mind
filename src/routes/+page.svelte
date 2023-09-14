1bf05db28e03cdd99fdf19c50762cbf5acb181cc
Frank Oppervoll-Larsen <frlarsen@microsoft.com>
2023-09-14 21:59:46 +0200

    Use filepicker to open org file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,81 +2,21 @@
 	import Mindmap from '$lib/mindmap.svelte';
 	import { orgTextToMindMap } from '$lib/orgTextToMindMap';
 
-	const orgText = `
-	#+TITLE: Org-mode mind
-#+AUTHOR: @crazyfraggle
-#+DATE: <2023-09-12>
+	let orgText = `#+TITLE: Org Mode Mindmap
+	
+* You need to click that "Open file" button to load a file.`;
 
-* Idea
-Since org-mode files are hierarchical, we can use them as a mind map.
+	$: orgTree = orgTextToMindMap(orgText);
 
-* Learnings
-** [ ] Can Svelte be an electron app?
-** [X] Vitest is great at testing.
-** Svelte is great at building.
-** Co-pilot is great at writing code.
-** Co-pilot like to brag about itself.
-** Co-pilot is awesome at writing tests.
-
-* New things tested out
-** vitest
-Snappy and easy to use.
-Co-pilot happily created actual usable tests.
-
-** css variables
-Allows passing CSS props from top level containers into child components without the need for prop drilling.
-
-** Svelte
-Nah, done that before. Just needs pointing out.
-
-** VSCode Org mode
-It sux.
-
-* Todos
-** Table support
-We need to support this:
-| Tables        | Are           | Cool  |
-| ------------- |:-------------:| -----:|
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      | centered      |   $12 |
-| zebra stripes | are neat      |    $1 |
-** Pre-formatted text
-Some pre-formatted text:
-#+BEGIN_EXAMPLE
-** This is a headline
-#+END_EXAMPLE
-
-** Code blocks
-#+BEGIN_SRC javascript
-const a = 1;
-#+END_SRC
-
-** Links
-[[https://www.google.com][Google]]
-
-** Images
-[[file:./images/2021-08-22-16-00-00.png]]
-
-** Lists
-
-*** Unordered
-- List item 1
-- List item 2
-
-+ List item 1
-+ List item 2
-  - Sub list item
-
-*** Ordered
-1. List item 1
-2. List item 2
-
-
-	`;
-
-	const orgTree = orgTextToMindMap(orgText);
+	async function openFile() {
+		const [fileHandle] = await window.showOpenFilePicker();
+		const file = await fileHandle.getFile();
+		const contents = await file.text();
+		orgText = contents;
+	}
 </script>
 
 <div>
+	<button on:click={openFile}>Open file</button>
 	<Mindmap orgtree={orgTree} />
 </div>
